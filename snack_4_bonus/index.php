8f58e95e8b5b7dc5adbd907569bce29d0e650d12
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-03 20:25:57 +0200

    bonus completato

--- snack_4_bonus/index.php
+++ snack_4_bonus/index.php
@@ -1,7 +1,7 @@
 <?php
     $randomArr = array();
     while(count($randomArr) < 15){
-        $newnum = rand(1, 15);
+        $newnum = rand(1, 99);
         if(!in_array($newnum, $randomArr)){
             $randomArr[] = $newnum;
         }
